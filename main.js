a3011e5b49bee217473af168b423b0e037a63a31
mariareis0 <106742987+mariareis0@users.noreply.github.com>
2024-04-16 09:45:32 -0300

    hgvhgvh

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-const botoes = document.querySelectorAll(".botao"); //esta linha seleciona todos os elementos HTML que tem a classe 'botao' e armazena-os na constante 'botoes'//
-for (let i = 0; i < botoes.length; i++) { //este é um loop que percorre cada botao na constante 'botoes'//
-    botoes[i].onclick = function (index){ //aqui, estamos atribuindo uma função ao evento 'onclick' de cada botão. Isso significa que a função será executada sempre que o botão for clicado//
-        for (let j = 0; j < botoes.length; j++) { //dentro da função 'onclick', temos outro loop que percorre cada botão novamente*/
-            botoes[j].classList.remove("ativo"); //aqui removemos a classe 'ativo' de cada botão. Isso pode ser usado para alterar o estilo do botão, por exemplo, para indicar qual botão está atualmente ativo//
+const botoes = document.querySelectorAll(".botao"); //constante botões
+const textos = document.querySelectorAll(".aba-conteudo"); //constante aba conteudo
+for (let i = 0; i < botoes.length; i++) { //laço de repetição que passará por todos botões
+    botoes[i].onclick = function (){ //funcao que ao clicar no botao ativa os efeitos e textos
+        for (let j = 0; j < botoes.length; j++) { 
+            botoes[j].classList.remove("ativo"); //aqui remove o efeito ativo do botao        
+            textos[j].classList.remove("ativo"); //aqui remove o efeito ativo do texto   
         }
-        botoes[i].classList.add("ativo"); //depois de remover a classe 'ativo' de todos os botões, adicionamos a classe 'ativo' de volta ao botão que foi clicado. Isso efetivamente garante que apenas o botão clicado tenha a classe 'ativo'//
+        botoes[i].classList.add("ativo"); //aqui ativa o efeito do botao
+        textos[i].classList.add("ativo"); //aqui ativa o texto no botao
     }
+}
+const contadores = document.querySelectorAll(".contador"); //constante contador
+const tempo1 = new Date("2024-12-13T00:00:00"); //estipula uma data para a contagem regressiva
+
+contadores[0].textContent = calculaTempo(tempo1); 
+function calculaTempo(tempo){ //funcao contagem regressiva
+    let tempoAtual = new Date(); 
+    let tempoFinal = tempo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
